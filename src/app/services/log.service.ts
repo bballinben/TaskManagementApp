9b716d1fded60221a5ae65bb610d71adfef34f56
bballinben <benjaminking106@gmail.com>
2018-04-02 00:42:53 -0700

    added observable for logs

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
 import { Log } from '../models/log';
 
 @Injectable()
 export class LogService {
   logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+
+  selectedLog = this.logSource.asObservable();
 
   constructor() { 
     this.logs = [
@@ -13,7 +21,11 @@ export class LogService {
     ]
    }
 
-   getLogs() {
-     return this.logs;
+   getLogs(): Observable<Log[]> {
+     return of(this.logs);
+   }
+
+   setFormLog(log: Log) {
+     this.logSource.next(log);
    }
 }
