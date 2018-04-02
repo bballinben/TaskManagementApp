a2b5ac19e5cdd2f87b5b4f1ff44d80961705a773
bballinben <benjaminking106@gmail.com>
2018-04-02 02:02:03 -0700

    added clear button functionality and titles to list

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -13,12 +13,18 @@ export class LogService {
 
   selectedLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean> (true);
+  stateClear = this.stateSource.asObservable();
+
   constructor() { 
-    this.logs = [
-      {id: '1', text: 'Task 1', date: new Date('12/26/2017 12:54:23')},
-      {id: '2', text: 'Task 2', date: new Date('12/28/2017 12:54:23')},
-      {id: '3', text: 'Task 3', date: new Date('12/30/2017 12:54:23')}
-    ]
+    // Test Logs
+    // this.logs = [
+    //   {id: '1', text: 'Task 1', date: new Date('12/26/2017 12:54:23')},
+    //   {id: '2', text: 'Task 2', date: new Date('12/28/2017 12:54:23')},
+    //   {id: '3', text: 'Task 3', date: new Date('12/30/2017 12:54:23')}
+    // ]
+
+    this.logs = [];
    }
 
    getLogs(): Observable<Log[]> {
@@ -47,6 +53,10 @@ export class LogService {
      if(log.id === cur.id) {
        this.logs.splice(index, 1);
      }
-    })
+    });
   }
+
+    clearState() {
+      this.stateSource.next(true);
+    }
 }
